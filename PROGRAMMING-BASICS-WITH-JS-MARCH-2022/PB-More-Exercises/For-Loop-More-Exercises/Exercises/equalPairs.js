0cf06ef6231684438bf4fe90cb1d57f37c77f60f
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2022-03-26 13:52:15 +0200

    Update

--- PROGRAMMING-BASICS-WITH-JS-MARCH-2022/PB-More-Exercises/For-Loop-More-Exercises/Exercises/equalPairs.js
+++ PROGRAMMING-BASICS-WITH-JS-MARCH-2022/PB-More-Exercises/For-Loop-More-Exercises/Exercises/equalPairs.js
@@ -2,18 +2,16 @@ function equalPairs(input) {
 
     let countNumbers = Number(input[0]) * 2;
 
-    let firstNum = 0;
-    let secondNum = 0;
-    let thirdNum = 0;
-    let fourthNum = 0;
+    let firstNumber = 0;
+    let secondNumber = 0;
+    let thirdNumber = 0;
+    let fourthNumber = 0;
 
-    let sum = 0;
+    let sum1 = 0;
     let sum2 = 0;
 
     let diff = 0;
     let maxDiff = 0;
-
-    let equalSum = true;
 
     if (countNumbers === 2) {
         console.log(`Yes, value=${Number(input[1]) + Number(input[2])}`);
@@ -21,38 +19,25 @@ function equalPairs(input) {
 
     for (let i = 1; i <= countNumbers; i += 2) {
 
-        firstNum = Number(input[i]);
-        secondNum = Number(input[i + 1]);
+        firstNumber = Number(input[i]);
+        secondNumber = Number(input[i + 1]);
+        sum1 = firstNumber + secondNumber;
 
-        if (i === (countNumbers - 1)) {
-            sum = sum2;
-            sum2 = firstNum + secondNum;
-        } else {
-            thirdNum = Number(input[i + 2]);
-            fourthNum = Number(input[i + 3]);
-            sum = firstNum + secondNum;
-            sum2 = thirdNum + fourthNum;
-        }
+        thirdNumber = Number(input[i+2]);
+        fourthNumber = Number(input[i+3]);
+        sum2 = thirdNumber + fourthNumber;
 
-        if (sum === sum2) {
-            sum2 = firstNum + secondNum;
-        } else {
-            diff = Math.abs(sum - sum2);
+        maxDiff = Math.abs(sum1 - sum2);
 
-            if (diff > maxDiff) {
-                maxDiff = diff;
-            }
-            equalSum = false;
-        }
-    }
-
-    if (equalSum === true) {
-        console.log(`Yes, value=${firstNum + secondNum}`);
-    } else {
+       if (sum1 === sum2) {
+        console.log(`Yes, value=${firstNumber + secondNumber}`);
+       } else {
         console.log(`No, maxdiff=${maxDiff}`);
+       }
     }
+
 }
-equalPairs(["3", "1", "2", "0", "3", "4", "-1"]);
+//equalPairs(["3", "1", "2", "0", "3", "4", "-1"]);
 equalPairs(["4", "1", "1", "3", "1", "2", "2", "0", "0"]);
 equalPairs(["2", "-1", "0", "0", "-1"]);
 equalPairs(["2", "1", "2", "2", "2"]);
