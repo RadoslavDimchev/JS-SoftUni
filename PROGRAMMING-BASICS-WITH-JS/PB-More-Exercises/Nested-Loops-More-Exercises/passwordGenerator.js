e8f30f67f8e4eae8f56ed482e031ad21ac25ecdb
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2022-04-06 00:58:36 +0300

    Nested-Loops-More-Exercises

--- PROGRAMMING-BASICS-WITH-JS/PB-More-Exercises/Nested-Loops-More-Exercises/passwordGenerator.js
+++ PROGRAMMING-BASICS-WITH-JS/PB-More-Exercises/Nested-Loops-More-Exercises/passwordGenerator.js
@@ -1,14 +1,14 @@
 function passwordGenerator(input) {
 
     let n = Number(input[0]);
-    let l = Number(input[1]);
+    let l = Number(input[1]) + 97;
 
     let passwords = "";
 
     for (let i = 1; i <= n; i++) {
         for (let j = 1; j <= n; j++) {
-            for (let k = 1; k <= l; k++) {
-                for (let a = 1; a <= l; a++) {
+            for (let k = 97; k < l; k++) {
+                for (let a = 97; a < l; a++) {
                     for (let b = 1; b <= n; b++) {
                         if (b > i && b > j) {
                             passwords += `${i}${j}${String.fromCharCode(k)}${String.fromCharCode(a)}${b} `;
@@ -22,6 +22,6 @@ function passwordGenerator(input) {
 
 }
 passwordGenerator(["2", "4"]);
-//passwordGenerator(["3", "1"]);
-//passwordGenerator(["3", "2"]);
-//passwordGenerator(["4", "2"]);
+passwordGenerator(["3", "1"]);
+passwordGenerator(["3", "2"]);
+passwordGenerator(["4", "2"]);
