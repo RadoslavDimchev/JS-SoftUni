d84ef533f1b623ea8e7398e4eae4c8145dc0ce3d
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2022-06-05 11:46:42 +0300

    Added last exercise from Objects - Exercise - Prep

---
+++ Programming-Fundamentals-with-JS/JS-Fundamentals-Preparation/Objects-and-Classes-Exercise/09.Catalogue.js
@@ -0,0 +1,41 @@
+function catalogue(array) {
+    let products = [];
+    for (let tokens of array) {
+        let info = tokens.split(' : ');
+        let name = info[0];
+        let price = Number(info[1]);
+        let product = {
+            name,
+            price
+        };
+        products.push(product);
+    }
+
+    products.sort((a, b) => a.name.localeCompare(b.name));
+
+    let pastLetter = '';
+    for (let currentProduct of products) {
+        let currentLetter = currentProduct.name.charAt(0).toUpperCase();
+        if (currentLetter === pastLetter) {
+            console.log(`  ${currentProduct.name}: ${currentProduct.price}`);
+        } else {
+            pastLetter = currentLetter;
+            console.log(currentLetter);
+            console.log(`  ${currentProduct.name}: ${currentProduct.price}`);
+        }
+    }
+}
+
+catalogue([
+    'Appricot : 20.4',
+    'Fridge : 1500',
+    'TV : 1499',
+    'Deodorant : 10',
+    'Boiler : 300',
+    'Apple : 1.25',
+    'Anti-Bug Spray : 15',
+    'T-Shirt : 10']);
+catalogue([
+    'Omlet : 5.4',
+    'Shirt : 15',
+    'Cake : 59']);
