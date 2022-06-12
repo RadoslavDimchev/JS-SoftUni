7a4e4882f20a3475cda40b108427de6e1d6c06fb
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2022-06-12 23:11:29 +0300

    Update factorialDivision and added another solution

--- Programming-Fundamentals-with-JS/JS-Fundamentals/Exercises/04-Functions/factorialDivision.js
+++ Programming-Fundamentals-with-JS/JS-Fundamentals/Exercises/04-Functions/factorialDivision.js
@@ -1,18 +1,34 @@
 function factorialDivision(firstNum, secondNum) {
-    let factorial = 1;
-
-    let result = (getFactorial(firstNum) / getFactorial(secondNum, factorial = 1)).toFixed(2);
+    let result = (getFactorial(firstNum) / getFactorial(secondNum)).toFixed(2);
     return result;
 
     function getFactorial(number) {
-        if (number > 0) {
-            factorial *= number;
-            getFactorial(number - 1);
+        if (number === 0 || number === 1) {
+            return 1;
+        } else {
+            let factorial = number * getFactorial(number - 1);
+            return factorial;
         }
-
-        return factorial;
     }
 }
+
+// second solution
+
+// function factorialDivision(firstNum, secondNum) {
+//     let firstNumFactorial = 1;
+//     let secondNumFactorial = 1;
+
+//     for (let i = 1; i <= firstNum; i++) {
+//         firstNumFactorial *= i;
+//     }
+
+//     for (let i = 1; i <= secondNum; i++) {
+//         secondNumFactorial *= i;
+//     }
+
+//     let result = (firstNumFactorial / secondNumFactorial).toFixed(2);
+//     return result;
+// }
 
 console.log(factorialDivision(5, 2));
 console.log(factorialDivision(6, 2));
