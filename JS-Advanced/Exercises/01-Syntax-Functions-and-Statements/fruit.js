bb65debc67d8017165d283323694de54751de688
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2022-09-14 23:48:36 +0300

    Updated fruit task from first exercise

--- JS-Advanced/Exercises/01-Syntax-Functions-and-Statements/fruit.js
+++ JS-Advanced/Exercises/01-Syntax-Functions-and-Statements/fruit.js
@@ -2,7 +2,7 @@ function fruit(fruitType, weightGrams, pricePerKg) {
   const weightKg = weightGrams / 1000;
   const money = weightKg * pricePerKg;
 
-  console.log(`I need ${money.toFixed(2)} to buy ${weightKg.toFixed(2)} kilograms ${fruitType}.`);
+  console.log(`I need $${money.toFixed(2)} to buy ${weightKg.toFixed(2)} kilograms ${fruitType}.`);
 }
 
 fruit('orange', 2500, 1.80);
